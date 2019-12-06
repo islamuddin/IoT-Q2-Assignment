8eccae629876970081d8b0723187b408a6769f55
idkhanchachar <uddinislam46@yahoo.com>
2019-12-06 15:56:02 +0500

    adding project deve project

---
+++ dev-project/target/debug/deps/dev_project-a9d84a7df30adecc.d
@@ -0,0 +1,5 @@
+D:\_piaic\course\IoT-Q2-Assignment\dev-project\target\debug\deps\dev_project-a9d84a7df30adecc.exe: src\main.rs
+
+D:\_piaic\course\IoT-Q2-Assignment\dev-project\target\debug\deps\dev_project-a9d84a7df30adecc.d: src\main.rs
+
+src\main.rs:
---
+++ dev-project/target/debug/dev-project.d
@@ -0,0 +1,1 @@
+D:\_piaic\course\IoT-Q2-Assignment\dev-project\target\debug\dev-project.exe: D:\_piaic\course\IoT-Q2-Assignment\dev-project\src\main.rs
